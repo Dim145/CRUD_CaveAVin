424ce66410fd3ed5cd0720c6e04c5bdf12a1b0f8
Florian Boireau <florian23russie@hotmail.fr>
2021-02-01 12:22:06 +0100

    Ajout des classes Appellation et Oenologue

--- web/classes/appellation.php
+++ web/classes/appellation.php
@@ -2,8 +2,35 @@
 
 class Appellation
 {
-    private int $id_appellation;
-    private String $nom_appellation;
-    private String $category_appellation;
+    private int    $id_appellation;
+    private string $nom_appellation;
+    private string $categorie_appellation;
+
+    /*public function __construct(int $id_appellation, string $nom_appellation, string $categorie_appellation)
+    {
+        $this->$id_appellation  = $id_appellation;
+        $this->$nom_appellation = $nom_appellation;
+        if($categorie_appellation == "Vin de table" || $categorie_appellation == "Vin de pays" || 
+           $categorie_appellation == "AOC/AOP"      || $categorie_appellation == "IGP"         ||
+           $categorie_appellation == "VDQS")
+            $this->$categorie_appellation = $categorie_appellation;
+        else
+            throw new Exception("Le paramètre categorie_appellation ne correspond au resultat attendu", 1);
+    }*/
+
+    public function get_id_appellation():int                   { return $this->$id_appellation; }
+    public function set_id_appellation(string $id_appellation) { $this->$id_appellation = $id_appellation; }
+
+    public function get_nom_appellation():string                 { return $this->$nom_appellation; }
+    public function set_nom_appellation(string $nom_appellation) { $this->$nom_appellation = $nom_appellation; }
+
+    public function get_categorie_appellation():string                             { return $this->$categorie_appellation; }
+    public function set_categorie_appellation(string $categorie_appid_appellation) { $this->$id_appellation = $id_appellation; }
+
+    public function __toString():string
+    {
+       return "id_appellation : ".$this->$id_appellation."  |  nom_appellation : ".$this->$nom_appellation."  | categorie_appellation : ".$this->$categorie_appellation;
+    }
 }
+
 ?>
---
+++ web/classes/oenologue.php
@@ -0,0 +1,15 @@
+<?php
+
+class Oenologue
+{
+    private int    $id_oenologue;
+    private string $nom_oenologue;
+
+    public function get_id_oenologue():int              { return $this->id_oenologue; }
+    public function set_id_oenologue(int $id_oenologue) { $this->id_oenologue = $id_oenologue; }
+
+    public function get_nom_oenologue():string            { return $this->nom_oenologue; }
+    public function set_nom_oenologue(int $nom_oenologue) { $this->nom_oenologue = $nom_oenologue; }
+
+    public function __toString():string { return "id_oenologue : ".$this->$id_oenologue."  |  nom_oenologue : ".$this->$nom_oenologue;}
+}
