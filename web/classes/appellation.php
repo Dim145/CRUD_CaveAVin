a068d923c13f7891e196b12e7e446df6d43c7c15
Dimitri Dubois <dimitri.bleach@gmail.com>
2021-02-01 12:44:24 +0100

    construct en moins

--- web/classes/appellation.php
+++ web/classes/appellation.php
@@ -6,18 +6,6 @@ class Appellation
     private string $nom_appellation;
     private string $categorie_appellation;
 
-    /*public function __construct(int $id_appellation, string $nom_appellation, string $categorie_appellation)
-    {
-        $this->$id_appellation  = $id_appellation;
-        $this->$nom_appellation = $nom_appellation;
-        if($categorie_appellation == "Vin de table" || $categorie_appellation == "Vin de pays" || 
-           $categorie_appellation == "AOC/AOP"      || $categorie_appellation == "IGP"         ||
-           $categorie_appellation == "VDQS")
-            $this->$categorie_appellation = $categorie_appellation;
-        else
-            throw new Exception("Le paramètre categorie_appellation ne correspond au resultat attendu", 1);
-    }*/
-
     public function get_id_appellation():int                   { return $this->id_appellation; }
     public function set_id_appellation(string $id_appellation) { $this->id_appellation = $id_appellation; }
 
